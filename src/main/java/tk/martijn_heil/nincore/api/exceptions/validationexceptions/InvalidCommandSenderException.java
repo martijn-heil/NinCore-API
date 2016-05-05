7e8edb609b9b5a55305237d0e78d5a666ed43df7
martijn-heil <m.heil375@gmail.com>
2016-05-05 18:48:19 +0200

    Fix MissingResourceException InvalidCommandSenderException

--- src/main/java/tk/martijn_heil/nincore/api/exceptions/validationexceptions/InvalidCommandSenderException.java
+++ src/main/java/tk/martijn_heil/nincore/api/exceptions/validationexceptions/InvalidCommandSenderException.java
@@ -11,7 +11,7 @@ public class InvalidCommandSenderException extends ValidationException
 {
     public InvalidCommandSenderException(CommandSender commandSender)
     {
-        super(commandSender, TranslationUtils.getStaticMsg(ResourceBundle.getBundle("tk.martijn_heil.api.res.messages",
+        super(commandSender, TranslationUtils.getStaticMsg(ResourceBundle.getBundle("tk.martijn_heil.nincore.api.res.messages",
                 NinCommandSender.fromCommandSender(commandSender).getMinecraftLocale().
                         toLocale()), "error.InvalidCommandSender"), null);
     }
